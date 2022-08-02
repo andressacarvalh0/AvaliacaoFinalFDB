371444139931a9a6b054efbae35df0ea82f79c52
Andressa Da Silva Carvalho <andressa.carvalho@redeflex.com.br>
2022-08-01 21:59:48 -0400

    Ataulização Config

---
+++ config_server/src/main/java/br/com/config_server/onfig_server/OnfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.config_server.onfig_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnfigServerApplication.class, args);
+	}
+
+}
---
+++ config_server/src/test/java/br/com/config_server/onfig_server/OnfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.config_server.onfig_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
